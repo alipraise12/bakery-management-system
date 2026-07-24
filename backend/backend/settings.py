fc1de6dec4fcde95f7d590e2a7c6d41f9a6de884
alipraise12 <alipraise60@gmail.com>
2026-07-24 16:06:59 -0700

    Configure WhiteNoise static storage

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -171,6 +171,12 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
