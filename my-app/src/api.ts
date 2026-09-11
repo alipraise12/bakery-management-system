1f7e133095ab3114aa4066f4fc3097f1cf445f39
alipraise12 <alipraise60@gmail.com>
2026-09-11 06:05:27 -0700

    Update api.ts

--- my-app/src/api.ts
+++ my-app/src/api.ts
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || "https://bakery-management-system.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL || "https://bakery-management-system-p1iv.onrender.com";
 
 export default API_URL;
