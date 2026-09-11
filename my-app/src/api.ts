ede56d88923fba197a65a5a07c4c786f970c2455
alipraise12 <alipraise60@gmail.com>
2026-09-11 05:40:49 -0700

    Update api.ts

--- my-app/src/api.ts
+++ my-app/src/api.ts
@@ -1,3 +1,3 @@
-const API_URL = "https://bakery-management-system-production-ece0.up.railway.app";
+const API_URL = import.meta.env.VITE_API_URL || "https://bakery-management-system.onrender.com";
 
 export default API_URL;
